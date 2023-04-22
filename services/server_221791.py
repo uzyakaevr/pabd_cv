872bdef0655c8ea5b16c4096858df321f9daaca4
221791 <ilya.zhabin17@gmail.com>
2023-04-22 16:54:03 +0300

    server added

---
+++ services/server_221791.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask('Image classifier')
+
+@app.route('/')
+def home():
+    return 'Home page'
+
+if __name__ == '__main__':
+    app.run(port=1791)
+    input()
