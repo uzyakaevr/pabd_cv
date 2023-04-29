5fe0644b8c333b66ca0cbfcaa14605cca8320f47
221791 <ilya.zhabin17@gmail.com>
2023-04-29 16:19:35 +0300

    test is ready

--- test/test.py
+++ test/test.py
@@ -1,16 +1,29 @@
+import unittest
+import requests
 import PIL.Image
-import sys
 import io
 from requests import request
 
-img = PIL.Image.open('../data/dog.jpg')
-type(img)
+class MyTestCase(unittest.TestCase):
+    def test_200(self):
+        response = requests.request('GET', 'http://localhost:1791/')
+        sample = response.content.decode()
+        self.assertEqual(sample, 'Home page')  # add assertion here
 
-buffer = io.BytesIO()
-img.save(buffer, format='JPEG')
+    def test_classify(self):
+        img = PIL.Image.open('../data/dog.jpg')
+        buffer = io.BytesIO()
+        img.save(buffer, format='JPEG')
 
-with buffer as buf:
-    buffer.seek(0)
-    response = request('POST', 'http://localhost:1234/classify', data=buf)
+        with buffer as buf:
+            buffer.seek(0)
+            response = request('POST', 'http://localhost:1791/classify', data=buf)
 
-print(response.content)
+        out = response.content.decode('utf-8')
+        expected = 'келли, Пембрук, Немецкая овчарка'
+        
+        self.assertEqual(out, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
