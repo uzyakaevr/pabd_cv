91722e7460c0c1d9c35aced2339b985305ac402b
221781 <venom1994@inbox.ru>
2023-05-13 11:14:00 +0300

    patch server_221781.py

--- services/server_221781.py
+++ services/server_221781.py
@@ -8,7 +8,7 @@ resnet = tf.keras.applications.ResNet101()
 with open('../data/imgnet_cats_ru.txt', encoding='utf-8') as f:
     cats = f.readlines()
 
-cats_ru = [s.rstrip() for s in cats]
+categories_ru = [s.rstrip() for s in cats]
 
 
 @app.route('/')
@@ -25,7 +25,7 @@ def classify():
     img_t = tf.image.resize(img_t, (224, 224))
     out = resnet(img_t)
     idxs = tf.argsort(out, direction='DESCENDING')[0][:3].numpy()
-    out = ', '.join([cats_ru[int(i)] for i in idxs])
+    out = ', '.join([categories_ru[int(i)] for i in idxs])
     return out
 
 
