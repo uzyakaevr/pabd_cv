24a8446f0262cbe8b49961dc35ebc59f27efe0e7
221788 <mianpyc@gmail.com>
2023-05-27 14:54:46 +0300

    process_data upd 27.05

--- src/process_data.py
+++ src/process_data.py
@@ -1,6 +1,6 @@
 import os.path
 import shutil
-
+import PIL.Image
 import click
 
 
@@ -13,37 +13,36 @@ import click
 @click.option('-i', '--in_dir', default='data\\raw\\kaggle')
 @click.option('-o', '--out_dir', default='data\\processed\\PetImages')
 @click.option('-n', '--n_img', default=20)
-def process_data(in_dir, out_dir, n_img):
-    # make_out_dir(out_dir)
-    copy_imgs(in_dir, out_dir, n_img)
+@click.option('-s', '--img_size', default=180)
+def process_data(in_dir, out_dir, n_img, img_size):
+    make_out_dir(out_dir)
+    process_imgs(in_dir, out_dir, n_img, img_size)
 
 
 def make_out_dir(out_dir):
     if os.path.exists(out_dir):
-        os.remove(out_dir)
-        os.mkdir(out_dir)
-        os.mkdir(os.path.join(out_dir, 'Cat'))
-        os.mkdir(os.path.join(out_dir, 'Dog'))
+        shutil.rmtree(out_dir)
+
+    os.mkdir(out_dir)
+    os.mkdir(os.path.join(out_dir, 'Cat'))
+    os.mkdir(os.path.join(out_dir, 'Dog'))
 
 
-def copy_imgs(in_dir, out_dir, n_img):
+def process_imgs(in_dir, out_dir, n_img, img_size):
     all_imgs = os.listdir(in_dir)
     cat_imgs = [img for img in all_imgs if img.startswith('cat')]
     dog_imgs = [img for img in all_imgs if img.startswith('dog')]
 
+    def resize_and_save(img_list, category_name):
+        for img_list in img_list[:n_img]:
+            in_img_path = os.path.join(in_dir, img_list)
+            img = PIL.Image.open(in_img_path)
+            img_r = img.resize((img_size, img_size))
+            out_img_path = os.path.join(out_dir, category_name, img_list)
+            img_r.save(out_img_path)
 
-    for cat_img in cat_imgs[:n_img]:
-        shutil.copy2(
-            os.path.join(in_dir, cat_img),
-            os.path.join(out_dir, "Cat")
-        )
-
-    for dog_img in dog_imgs[:n_img]:
-        shutil.copy2(
-            os.path.join(in_dir, dog_img),
-            os.path.join(out_dir, "Dog")
-        )
-
+    resize_and_save(cat_imgs, "Cat")
+    resize_and_save(dog_imgs, "Dog")
 
 if __name__ == '__main__':
     process_data()
